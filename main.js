c5114880597872dc56338ed0c91690f18438d8a6
advait1109 <86603536+advait1109@users.noreply.github.com>
2022-09-02 16:30:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 img="";
+statuses='';
 function preload(){
     img=loadImage('dog_cat.jpg');
 }
 function setup(){
     canvas=createCanvas(640,420,0,0);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML="Status: Detecting objects";
 }
 function draw(){
     image(img,0,0,640,420);
@@ -14,4 +17,23 @@ function draw(){
     noFill();
     stroke("red");
     rect(50,50,400,360);
+    fill("red");
+    textSize(18);
+    text("Cat",350,100);
+    noFill();
+    stroke('red');
+    rect(300,75,300,325);
+}
+function modelLoaded(){
+    console.log('Model has Loaded');
+    statuses=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if (error){
+        console.error(error);
+    }    
+    else{
+        console.log(results);
+    }
 }
